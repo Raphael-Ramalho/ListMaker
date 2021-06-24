ac13acc208a2f99973e8b89b21db5bb8475fa6c9
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-24 11:46:42 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,32 @@
 const submitButton = document.querySelector("[data-submit-button]")
 const textInput = document.querySelector("[data-text-input]")
+const itemsList = document.querySelector("[data-list]")
+const doneButton = document.querySelector("[data-done-button]")
+const deleteButton = document.querySelector("[data-delete-button]")
 
 
 
-const teste = (event) => {
-    const item = textInput.value;
+const createElement = (event) => {
     event.preventDefault();
-    console.log(item);
+
+    const itemBlock = document.createElement("li")
+
+    itemBlock.classList.add("item")
+
+    const itemContent = textInput.value;
+
+    itemBlock.innerHTML = `<p>${itemContent}</p>
+    <div class="buttons">
+    <button class="done" data-done-button>Done</button>
+    <button class="delete">Delete</button>
+    </div>` 
+
+    itemsList.appendChild(itemBlock)
+
+    console.log(itemsList)
+
 }
 
-submitButton.addEventListener("click", teste)
+submitButton.addEventListener("click", createElement)
 
 
