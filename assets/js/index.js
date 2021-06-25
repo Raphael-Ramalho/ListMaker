094412e5fa298c0e4344dcf6c411816147ae785c
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-25 00:40:07 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,65 @@
 const submitButton = document.querySelector("[data-submit-button]")
 const textInput = document.querySelector("[data-text-input]")
-const itemsList = document.querySelector("[data-list]")
+
+//const doneButton = document.querySelector("[data-done-button]")
+//const deleteButton = document.querySelector("[data-delete-button]")
 
 
 
+const createElement = () => {
 
-const createElement = (event) => {
-    event.preventDefault();
-
+    const itemsList = document.querySelector("[data-list]")
     const itemBlock = document.createElement("li")
+    const buttonBlock = document.createElement("div")
+    buttonBlock.classList.add("buttons")
     const itemContent = textInput.value;
 
     itemBlock.classList.add("item")
     itemBlock.setAttribute("data-item","")
-    itemBlock.innerHTML = `<p data-item-text>${itemContent}</p>
-    <div class="buttons">
-    <button class="done" data-done-button>Done</button>
-    <button class="delete" data-delete-button>Delete</button>
-    </div>` 
+    itemBlock.innerHTML = `<p data-item-text>${itemContent}</p>` 
+    itemBlock.appendChild(buttonBlock)
     itemsList.appendChild(itemBlock)
+    buttonBlock.appendChild(completed())
 
-    //Bloco para marcar item como concluido
-    const doneButton = document.querySelectorAll("[data-done-button]")
-    const numUltimoItem = doneButton.length - 1; //numUltimoItem é o numero do ultimo item criado no nodelist doneButton
 
-    const completed = () =>{
-        const textItem = document.querySelectorAll("[data-item-text]")
-        textItem[numUltimoItem].classList.toggle("completed")
-    }
-
-    doneButton[numUltimoItem].addEventListener("click", completed)
-
-    //Bloco para deletar item
-    const deleteButton = document.querySelectorAll("[data-delete-button]")
-
-    const deleteItem = () =>{
-        const item = document.querySelectorAll("[data-item]")
-        item[numUltimoItem].remove()
-    }
-
-    deleteButton[numUltimoItem].addEventListener("click", deleteItem)
-
+    //const botaoConclui = document.createElement("button");
 }
+
+const completed = () =>{
+    //const textItem = document.querySelector("[data-item-text]")
+    // textItem.classList.toggle("completed")
+
+    const doneButton = document.createElement("button")
+    doneButton.classList.add("done")
+    doneButton.innerText = "Done"
+    doneButton.addEventListener("click", toggleDone)
+    return doneButton
+}
+
+
+const toggleDone = (event) =>{
+    const donebutton = event.target
+    const buttonBlock = donebutton.parentNode
+    const textItem = buttonBlock.parentNode.firstChild
+    textItem.classList.toggle("completed")
+}
+
+
+
+// const deleteItem = () =>{
+//     const item = document.querySelectorAll("[data-item]")
+//     item[numUltimoItem].remove()
+// }
 
 
 submitButton.addEventListener("click", createElement)
 
 
 
+{/* <li>
+<p data-item-text>dsfgsdgsd</p>
+<div class="buttons">
+<button class="done" data-done-button>Done</button>
+<button class="delete" data-delete-button>Delete</button>
+</div> 
+</li> */}
