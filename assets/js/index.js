09eca460fe587bff92b2353ed4492679066257c7
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-01 20:23:15 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,11 +9,8 @@ const createElement = (event) => {
     event.preventDefault()
     const textInput = document.querySelector("[data-text-input]")
     const itemsList = document.querySelector("[data-list]")
-    const itemBlock = document.createElement("li")
-    const buttonBlock = document.createElement("div")
-    buttonBlock.classList.add("buttons")
     const itemContent = textInput.value;
-
+    itemsList.innerHTML = ""
 
     //armazenamentFunction(itemContent)
     const armazenamento = JSON.parse(localStorage.getItem("tarefa")) || []
@@ -21,25 +18,45 @@ const createElement = (event) => {
         itemContent
     }
     armazenamento.push(valoresArmazenados)
-
+    console.log(armazenamento.length)
     localStorage.setItem("tarefa", JSON.stringify(armazenamento))
     //
 
     
-
-    itemBlock.classList.add("item")
-    itemBlock.setAttribute("data-item","")
-    itemBlock.innerHTML = `<p data-item-text>${itemContent}</p>` 
-    itemBlock.appendChild(buttonBlock)
-    itemsList.appendChild(itemBlock)
-    buttonBlock.appendChild(doneButton())
-    buttonBlock.appendChild(deleteButton())
+    for(let i = 0; i <  armazenamento.length; i++){
+        const itemBlock = document.createElement("li")
+        const buttonBlock = document.createElement("div")
+        buttonBlock.classList.add("buttons")
+        itemBlock.classList.add("item")
+        itemBlock.innerHTML = `<p>${armazenamento[i].itemContent}</p>` 
+        itemBlock.appendChild(buttonBlock)
+        itemsList.appendChild(itemBlock)
+        buttonBlock.appendChild(doneButton())
+        buttonBlock.appendChild(deleteButton())
+    }
 
     textInput.value = ""
 }
 
 
 
+
 submitButton.addEventListener("click", createElement)
 
 
+
+
+// for(i = 0; i <  armazenamento.length; i++){
+//     const itemBlock = document.createElement("li")
+//     const buttonBlock = document.createElement("div")
+//     buttonBlock.classList.add("buttons")
+//     itemBlock.classList.add("item")
+//     itemBlock.setAttribute("data-item","")
+//     itemBlock.innerHTML = `<p data-item-text>${itemContent}</p>` 
+//     itemBlock.appendChild(buttonBlock)
+//     itemsList.appendChild(itemBlock)
+//     buttonBlock.appendChild(doneButton())
+//     buttonBlock.appendChild(deleteButton())
+// }
+
+// textInput.value = ""
